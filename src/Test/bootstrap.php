6eeb2ba0c22ce784d965f1dab722e5b0d63cc54e
Andreas Weber <code@andreas-weber.me>
2015-03-16 13:33:17 +0100

    Added file headers.

--- src/Test/bootstrap.php
+++ src/Test/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the andreas-weber/php-config library.
+ *
+ * (c) Andreas Weber <code@andreas-weber.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
